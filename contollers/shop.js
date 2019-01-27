4beeecf96c4db3c4aa3f6d0fc386093671365368
vican22 <vedranknezevic1@yahoo.com>
2019-01-27 16:54:05 +0100

    Add to Cart funcionality

--- contollers/shop.js
+++ contollers/shop.js
@@ -1,10 +1,22 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
+      path: "/products"
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
       path: "/products"
     });
   });
@@ -27,6 +39,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
