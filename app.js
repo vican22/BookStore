4decedcb59f25ee07144bcaa6cb96466ceaa4aa0
Vedran <vedranknezevic1@yahoo.com>
2019-01-29 13:10:08 +0100

    setting CRUD operations with db

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./util/database");
+const sequilize = require("./util/database");
 
 const errorController = require("./controllers/error");
 
@@ -21,5 +21,12 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequilize
+  .sync()
+  .then(result => {
+    //console.log(result);
+  })
+  .catch(err => console.log(err));
 
 app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequilize = require("sequelize");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "book-store",
-  password: "rootroot"
+const sequelize = new Sequilize("book-store", "root", "rootroot", {
+  dialect: "mysql",
+  host: "localhost"
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
