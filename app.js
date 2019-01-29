d6d5147dcd3f56a68f97bd89cb708c946dc558c3
Vedran <vedranknezevic1@yahoo.com>
2019-01-29 14:01:41 +0100

    creating user table and associating  with product table

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const sequilize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const errorController = require("./controllers/error");
 
@@ -17,16 +19,38 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+//User created this product not purchased it
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequilize
   .sync()
   .then(result => {
     //console.log(result);
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: "Vedran", email: "vedran@gmail.com" });
+    }
+    return Promise.resolve(user);
+  })
+  .then(user => {
+    //console.log(user);
+    app.listen(3000);
   })
   .catch(err => console.log(err));
-
-app.listen(3000);
