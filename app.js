8e829176af7f3e4d44c011c7563efe39ca1df648
Vedran <vedranknezevic1@yahoo.com>
2019-02-17 21:16:56 +0100

    setting cookies

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
+
+const mongoDB = require("./config/config");
 
 const app = express();
 
@@ -14,9 +17,13 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById("5c598dee8b849a2498228df2")
@@ -29,13 +36,12 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://vican22:123456vk@mojcluster-1d9m1.mongodb.net/shop?retryWrites=true"
-  )
+  .connect(mongoDB.auth)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
